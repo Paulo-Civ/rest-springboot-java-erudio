91a916960611b197b3c7055459c46a3491f8948f
Paulo Civardi <paulocivardi@gmail.com>
2024-07-05 18:27:07 -0400

    Content Negotiation

---
+++ 13_ContentNegotiation/rest-springboot-java-erudio/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Santa Catarina', 'Paulo', 'Male', 'Civardi'),
+	(2, 'Porto Alegre - Rio Grande do Sul', 'Lucas', 'Male', 'Civardi'),
+	(3, 'São Paulo', 'Matheus', 'Male', 'Civardi'),
+	(6, 'Kentucky - US', 'Muhammad', 'Male', 'Ali'),
+	(7, 'Portugal', 'Otávio', 'Male', 'Lima'),
+	(8, 'Portugal', 'Nicolas', 'Male', 'Lima');
